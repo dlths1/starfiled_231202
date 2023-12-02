0aa123fc443bed507b341765e8a8f3bdafbe7271
dlths1 <43879505+dlths1@users.noreply.github.com>
2023-12-02 13:46:26 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-let stars = []; //(x, y)
-let factor = 100;
-let speedSlider;
+let stars = []; //(x, y) //별 여러개 생성하기 위한 배열 객체
+let factor = 100; //
+let speedSlider; //속도 조절용 슬라이드
 let width, height;
 
 
@@ -8,14 +8,14 @@ function setup() {
   width = windowWidth; 
   height = windowHeight;
   createCanvas(width, height);
-  speedSlider = createSlider(0, 20, 10, 1);
+  speedSlider = createSlider(0, 20, 10, 1); //최소, 최대, 시작값, 간극
 
   for(let i = 0; i < 500; i++){
     stars[i] =  createVector(
       random(-width*factor, width*factor),
       random(-height*factor, height*factor),
-      random(width));
-    stars[i].pz = stars[i].z;
+      random(width)); //z는 속도를 의미
+    stars[i].pz = stars[i].z; //이전의 좌표
     //stars[i] = {x, y, z, pz};
 
   }
@@ -26,14 +26,15 @@ function setup() {
 function draw() {
   background(0, 0, 0);
   fill(255);
-  translate(width/2, height/2);
+  translate(width/2, height/2); //화면 가운데로 이동
 
   for(let star of stars){
-    let x = star.x/star.z;
+    let x = star.x/star.z; //속도를 나눈만큼 x좌표가 이동하도록
     let y = star.y/star.z;
     let px = star.x/star.pz;
     let py = star.y/star.pz;
     let d = map(star.z, 0, width, 10, 1);
+    //map(대상, 대상의 최소, 대상의 최대, 새로운 범주의 최소, 새로운 범주의 최대)
 
     fill(255, 200, 150);
   //translate(width/2, height/2);
